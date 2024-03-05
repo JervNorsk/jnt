782fa5dc292333c96718bb2b4e3d5f52caf1a3e1
JervNorsk <jerv.norsk@gmail.com>
2024-03-05 11:56:49 +0100

    Added JNT VTuber project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.projectDir
     .listFiles()
     ?.filter { it -> it.isDirectory }
     ?.filter { it -> it.name.equals("integrations") }
+    ?.filter { it -> it.name.equals("services") }
     ?.forEach {
         it.listFiles()
             ?.forEach { file ->
